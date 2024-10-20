a785e251b058730a5689ba95f220e67c4b61e0db
Olga Akperova <akperovao28@gmail.com>
2024-10-20 18:52:12 +0300

    hm2

--- src/task02.jl
+++ src/task02.jl
@@ -1,10 +1,22 @@
+export arraysum, mul35sum
+
 function arraysum(ar::AbstractVector{<:Number})::Float64
-    sum = 0;
+    sum=0; 
+    for i in ar
+            sum=sum+i;
+    end
     return sum;
 end
 
 
 function mul35sum(N::Integer)::Int64
-    sum = Int64(0);
-    return sum;
+    v=Vector{Float64}()
+    for i in 1:(N-1) 
+        if i%3==0
+            push!(v,Float64(i))
+        elseif i%5==0
+            push!(v,Float64(i))   
+        end
+    end
+    sum = arraysum(v);
 end
